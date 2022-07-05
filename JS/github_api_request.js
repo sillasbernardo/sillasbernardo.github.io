db8c7975e77810795e19117b4a8500bceb538a3f
sillasbernardo <syllasbernardo@gmail.com>
2022-07-05 15:24:43 -0300

    commit

--- JS/github_api_request.js
+++ JS/github_api_request.js
@@ -29,45 +29,45 @@ gh_access.rest.repos.listForUser({
     for (let index = 0; index < res.data.length; index++){
         /* DECLARING VALUES TO ARRAYS - START */
 
-        console.log(res.data[index]);
-
         images[index] = `https://raw.githubusercontent.com/${res.data[index].owner.login}/${res.data[index].name}/main/img/avatar.jpg`;
 
-        /* div 'title' */
-        repo_div_title[index] = document.createElement('div'); // Creates div element
-        repo_div_title[index].className = 'title'; // Set the div class name to 'images'
-        
-        /* h4 element > div 'title' */
-        repo_details_title[index] = document.createElement('h4'); // Creates h4 element
-        repo_details_title[index].textContent = res.data[index].name; // Set the h4 content text
-
-        /* div 'details' */
-        repo_div_details[index] = document.createElement('div'); // Creates div element
-        repo_div_details[index].className = 'details'; // Set the div class name to 'details
-
-        /* p element > div 'details' */
-        repo_details_desc[index] = document.createElement('p'); // Creates p element
-        repo_details_desc[index].textContent = res.data[index].description;
-
-        /* btn element > div 'details' */
-        view_project_btn[index] = document.createElement('a'); // Creates button
-        view_project_btn[index].className = 'view-more'; // Set the button class name to 'view-more'
-        view_project_btn[index].textContent = 'View more'; // Set text content
-        view_project_btn[index].setAttribute('href', `${res.data[index].html_url}`);
-        
-        /* DECLARING VALUES TO ARRAYS - END */
-
-
-        /* ADDING ELEMENTS TO DIVS - START*/
-        document.getElementById('portfolio').appendChild(repo_div_title[index]); // Add div 'images' to existing div
-        repo_div_title[index].appendChild(repo_details_title[index]); // Add title to repo_div_details
-
-        document.getElementById('portfolio').appendChild(repo_div_details[index]); // Add div 'details' to existing div      
-
-        /* If no image is found on github, then js won't create 'img' element in the DOM */
+        /* Repo without images won't be loaded */
         fetch(images[index])
             .then(response => {
                 if (response.ok){
+            
+                    /* div 'title' */
+                    repo_div_title[index] = document.createElement('div'); // Creates div element
+                    repo_div_title[index].className = 'title'; // Set the div class name to 'images'
+                    
+                    /* h4 element > div 'title' */
+                    repo_details_title[index] = document.createElement('h4'); // Creates h4 element
+                    repo_details_title[index].textContent = res.data[index].name; // Set the h4 content text
+            
+                    /* div 'details' */
+                    repo_div_details[index] = document.createElement('div'); // Creates div element
+                    repo_div_details[index].className = 'details'; // Set the div class name to 'details
+            
+                    /* p element > div 'details' */
+                    repo_details_desc[index] = document.createElement('p'); // Creates p element
+                    repo_details_desc[index].textContent = res.data[index].description;
+            
+                    /* btn element > div 'details' */
+                    view_project_btn[index] = document.createElement('a'); // Creates button
+                    view_project_btn[index].className = 'view-more'; // Set the button class name to 'view-more'
+                    view_project_btn[index].textContent = 'View more'; // Set text content
+                    view_project_btn[index].setAttribute('href', `${res.data[index].html_url}`);
+                    
+                    /* DECLARING VALUES TO ARRAYS - END */
+            
+            
+                    /* ADDING ELEMENTS TO DIVS - START*/
+                    document.getElementById('portfolio').appendChild(repo_div_title[index]); // Add div 'images' to existing div
+                    repo_div_title[index].appendChild(repo_details_title[index]); // Add title to repo_div_details
+            
+                    document.getElementById('portfolio').appendChild(repo_div_details[index]); // Add div 'details' to existing div      
+            
+
                     /* img element > div 'details' */
                     repo_images[index] = document.createElement('img'); // Creates img element
                     repo_images[index].src = images[index];
